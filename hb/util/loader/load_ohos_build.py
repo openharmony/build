632cdd9284bc785061fefbf1833aa4bd5c634a5b
keminLuo <luojianming4@h-partners.com>
2023-08-22 11:35:15 +0800

    修复xts子系统部件device_attest编译 Signed-off-by: 罗键铭 <541416002@qq.com>

--- hb/util/loader/load_ohos_build.py
+++ hb/util/loader/load_ohos_build.py
@@ -577,23 +577,6 @@ class LoadBuildConfig(object):
     def parts_info_filter(self, save_part):
         if save_part is None:
             raise Exception
-        print("[kemin] get_parts_info _parts_variants")
-        print(self._parts_variants)
-        print("[kemin] get_parts_info _part_targets_label")
-        print(self._part_targets_label)
-        print("[kemin] get_parts_info _parts_info_dict")
-        print(self._parts_info_dict)
-        print("[kemin] get_parts_info _phony_targets")
-        print(self._phony_targets)
-        print("[kemin] get_parts_info _parts_path_dict")
-        print(self._parts_path_dict)
-        print("[kemin] get_parts_info _part_hisysevent_config")
-        print(self._part_hisysevent_config)
-        print("[kemin] get_parts_info _parts_module_list")
-        print(self._parts_module_list)
-        print("[kemin] get_parts_info _parts_deps
-        print(self._parts_deps)
-        print("[kemin] get_parts_info ==========================")
         for key in list(self._parts_variants.keys()):
             if key not in save_part:
                 del self._parts_variants[key]
@@ -621,23 +604,6 @@ class LoadBuildConfig(object):
         for key in list(self._parts_deps.keys()):
             if key not in save_part:
                 del self._parts_deps[key]
-        print("[kemin] get_parts_info _parts_variants")
-        print(self._parts_variants)
-        print("[kemin] get_parts_info _part_targets_label")
-        print(self._part_targets_label)
-        print("[kemin] get_parts_info _parts_info_dict")
-        print(self._parts_info_dict)
-        print("[kemin] get_parts_info _phony_targets")
-        print(self._phony_targets)
-        print("[kemin] get_parts_info _parts_path_dict")
-        print(self._parts_path_dict)
-        print("[kemin] get_parts_info _part_hisysevent_config")
-        print(self._part_hisysevent_config)
-        print("[kemin] get_parts_info _parts_module_list")
-        print(self._parts_module_list)
-        print("[kemin] get_parts_info _parts_deps
-        print(self._parts_deps)
-        print("[kemin] get_parts_info ==========================")
 
 
 def compare_subsystem_and_component(subsystem_name, components_name, subsystem_compoents_whitelist_info,
@@ -868,9 +834,11 @@ def get_parts_info(source_root_dir,
                                        load_test_config, overrided_components,
                                        bundle_subsystem_allow_list)
         # xts subsystem special handling, device_attest and
-        # device_attest_lite parts need to be compiled into the version image, other parts are not
+        # device_attest_lite parts need to be compiled into the version image, other parts are not.
+        # parts_modules_info needs to be parse before filting.
         if subsystem_name == 'xts' and build_xts is False:
             xts_device_attest_name = ['device_attest_lite', 'device_attest']
+            build_loader.parse()
             build_loader.parts_info_filter(xts_device_attest_name)
         _parts_variants = build_loader.parts_variants()
         parts_variants.update(_parts_variants)
