f5a8361220022a24455632358ca3c0a177b0f0e6
zhongning5 <zhongning5@huawei.com>
2023-09-07 16:52:09 +0800

    添加nwebspawn 到白名单
    Signed-off-by: zhongning5 <zhongning5@huawei.com>
    Change-Id: Ic894450ff1287a6d44704162c34fe040f1e3caa0

--- ohos/packages/check_seccomp_library_name.py
+++ ohos/packages/check_seccomp_library_name.py
@@ -56,7 +56,7 @@ def collect_cfg_services_name(cfg_dir):
 
 def collect_seccomp_services_name(lib_dir):
     services_name = set()
-    name_allow_list = ['system', 'app', 'renderer']
+    name_allow_list = ['system', 'app', 'renderer', 'nwebspawn']
     if not os.path.exists(lib_dir):
         return services_name
     for file in os.listdir(lib_dir):
