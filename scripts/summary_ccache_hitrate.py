d5bbf44de6f1c37dfb6d892a50ae7cb18bff58c4
zhengweiwei <zhengweiwei11@huawei.com>
2023-09-26 10:39:13 +0800

    print ccache size and version after compile
    Issue:https://gitee.com/openharmony/build/issues/I84DL4
    Signed-off-by: zhengweiwei <zhengweiwei11@huawei.com>

--- scripts/summary_ccache_hitrate.py
+++ scripts/summary_ccache_hitrate.py
@@ -48,6 +48,26 @@ def summary_ccache_new(ccache_log):
     return hit_rate, mis_rate, hit_dir_num, hit_pre_num, mis_num
 
 
+def summary_ccache_size():
+    cache_size, ccache_version = "", ""
+    ccache_exec = os.environ.get('CCACHE_EXEC')
+    ccache_dir = os.environ.get('CCACHE_DIR')
+    print("ccache_dir = {}, ccache_exec = {}".format(ccache_dir, ccache_exec))
+    if ccache_exec is None or ccache_dir is None:
+        print("ccache_exec or ccache_dir is None, summary ccache size failed...")
+        return cache_size, ccache_version
+    cmd = [ccache_exec, '-s', '-V']
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0].decode("utf-8").split("\n")
+    cache_size_str = "Cache size"
+    ccache_version_str = "ccache version"
+    for line in process:
+        if cache_size_str in line:
+            cache_size = line.split(":")[1].strip()
+        if ccache_version_str in line:
+            ccache_version = line.split(" ")[2].strip()
+    return cache_size, ccache_version
+
+
 def main():
     if len(sys.argv) < 2:
         print("Error, please input the ccache log file path.")
@@ -55,22 +75,27 @@ def main():
 
     ccache_log = sys.argv[1]
     hit_rate = 0
-    mis_rate = 0
+    miss_rate = 0
     hit_dir_num = 0
     hit_pre_num = 0
-    mis_num = 0
+    miss_num = 0
+    cache_size = ""
+    ccache_version = ""
     if os.path.exists(ccache_log):
-        hit_rate, mis_rate, hit_dir_num, hit_pre_num, mis_num = summary_ccache_new(
+        hit_rate, miss_rate, hit_dir_num, hit_pre_num, miss_num = summary_ccache_new(
             ccache_log)
+        cache_size, ccache_version = summary_ccache_size()
 
-    print("---------------------------------------------")
-    print("ccache summary:")
-    print("cache hit (direct)  : " + str(hit_dir_num))
-    print("cache hit (preprocessed)  : " + str(hit_pre_num))
-    print("cache miss  : " + str(mis_num))
-    print("hit rate:  %.2f%% " % (hit_rate * 100))
-    print("mis rate: %.2f%% " % (mis_rate * 100))
-    print("---------------------------------------------")
+    print(f"--------------------------------------------\n" +
+          "ccache summary:\n" +
+          "ccache version: " + ccache_version + "\n" +
+          "cache hit (direct): " + str(hit_dir_num) + "\n" +
+          "cache hit (preprocessed): " + str(hit_pre_num) + "\n" +
+          "cache miss: " + str(miss_num) + "\n" +
+          "hit rate: %.2f%% " % (hit_rate * 100) + "\n" +
+          "miss rate: %.2f%% " % (miss_rate * 100) + "\n" +
+          "Cache size (GB): " + cache_size + "\n" +
+          "---------------------------------------------")
 
 
 if __name__ == "__main__":
