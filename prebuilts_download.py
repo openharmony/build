eb65c322071e82da683dd7331dfc2e1ca1b5c343
马广生 <maguangsheng1@huawei.com>
2023-11-10 03:43:25 +0000

    npm包自动更新锁定
    Signed-off-by: 马广生 <maguangsheng1@huawei.com>
    Change-Id: I702fa3f20fe234bccaaa267ba545d5d32529be87

--- prebuilts_download.py
+++ prebuilts_download.py
@@ -196,7 +196,7 @@ def _npm_install(args):
         skip_ssl_cmd = '{} config set strict-ssl false;'.format(npm)
         _run_cmd(skip_ssl_cmd)
     npm_clean_cmd = '{} cache clean -f'.format(npm)
-    npm_package_lock_cmd = '{} config set package-lock false'.format(npm)
+    npm_package_lock_cmd = '{} config set package-lock true'.format(npm)
     _run_cmd(npm_clean_cmd)
     _run_cmd(npm_package_lock_cmd)
     print('start npm install, please wait.')
