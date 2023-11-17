7e84febfadd31baeb1b11714ba62693c1b2e3b8e
lphwork <liupenghui12@huawei.com>
2023-11-17 16:46:03 +0800

    Issue: https://gitee.com/openharmony/build/issues/I8GDTW
    Comment: fix bug start_ex.sh file
    
    Signed-off-by: lphwork <liupenghui12@huawei.com>

--- test/script/start_ex.sh
+++ test/script/start_ex.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+root_path=""
+generate_build_dir(){
+  script_path=$(cd $(dirname $0);pwd)
+  pre_dir_path=$(dirname ${script_path})
+  pre_dir_path=$(dirname ${pre_dir_path})
+  root_path=$(dirname ${pre_dir_path})
+
+  mkdir -p $root_path/out/rk3568/obj/build
+}
+
 generate_build_option_report() {
     build_option_example_file=$1
-    pytest -vs --html ../out/build_option_report.html ../example/$build_option_example_file
-    pkill -f '/pyd.py --root /root/.pycache --start'
+    pytest -vs --html $root_path/out/rk3568/obj/build/build_option_report.html $root_path/build/test/example/$build_option_example_file
+    pkill -f '/pyd.py --root .*/.pycache --start'
 }
 
 generate_gn_template_report() {
     gn_template_example_file=$1
-    pytest -vs --html ../out/gn_template_report.html ../example/$gn_template_example_file
+    pytest -vs --html $root_path/out/rk3568/obj/build/gn_template_report.html $root_path/build/test/example/$gn_template_example_file
 }
+
 
 start() {
     if [[ $# -eq 2 && $1 == 'test_build_option.py' && $2 == 'test_gn_template.py' ]]; then
@@ -24,6 +35,7 @@ start() {
     fi
 }
 
+generate_build_dir
 start "$@"
 
 
