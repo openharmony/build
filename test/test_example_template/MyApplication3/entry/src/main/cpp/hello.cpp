ff2b0b48d139c1a637da1709e5ed9fdaf77d587c
lphwork <liupenghui12@huawei.com>
2023-11-30 15:58:06 +0800

    Issue: https://gitee.com/openharmony/build/issues/I8GAP9
    Comment: Fix Bug
    
    Signed-off-by: lphwork <liupenghui12@huawei.com>

--- test/test_example_template/MyApplication3/entry/src/main/cpp/hello.cpp
+++ test/test_example_template/MyApplication3/entry/src/main/cpp/hello.cpp
@@ -13,7 +13,6 @@
  * limitations under the License.
  */
 
-
 #include "napi/native_api.h"
 #include "add.h"
 
@@ -23,7 +22,7 @@ static napi_value Add(napi_env env, napi_callback_info info)
     size_t argc = 2;
     napi_value args[2] = {nullptr};
 
-    napi_get_cb_info(env, info, &argc, args , nullptr, nullptr);
+    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
 
     napi_valuetype valuetype0;
     napi_typeof(env, args[0], &valuetype0);
@@ -41,28 +40,26 @@ static napi_value Add(napi_env env, napi_callback_info info)
     napi_create_double(env, add_test(value0, value1), &sum);
 
     return sum;
-
 }
 
 EXTERN_C_START
 static napi_value Init(napi_env env, napi_value exports)
 {
     napi_property_descriptor desc[] = {
-        { "add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr }
-    };
+        {"add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr}};
     napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
     return exports;
 }
 EXTERN_C_END
 
 static napi_module demoModule = {
-    .nm_version =1,
+    .nm_version = 1,
     .nm_flags = 0,
     .nm_filename = nullptr,
     .nm_register_func = Init,
     .nm_modname = "entry",
-    .nm_priv = ((void*)0),
-    .reserved = { 0 },
+    .nm_priv = ((void *)0),
+    .reserved = {0},
 };
 
 extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
--- test/test_example_template/test_ohos_static_library/src/main.cpp
+++ test/test_example_template/test_ohos_static_library/src/main.cpp
@@ -13,36 +13,37 @@
  * limitations under the License.
  */
 
-
 #include <iostream>
 
 using namespace std;
- 
-class Adder{
-   public:
-      Adder(int i = 0)
-      {
-        total = i;
-      }
-      void addNum(int number)
-      {
-          total += number;
-      }
-      int getTotal()
-      {
-          return total;
-      };
-   private:
-      int total;
+
+class Adder
+{
+public:
+   Adder(int i = 0)
+   {
+      total = i;
+   }
+   void addNum(int number)
+   {
+      total += number;
+   }
+   int getTotal()
+   {
+      return total;
+   };
+
+private:
+   int total;
 };
-int main( )
+int main()
 {
    Adder a;
-   
+
    a.addNum(10);
    a.addNum(20);
    a.addNum(30);
- 
-   cout << "Total " << a.getTotal() <<endl;
+
+   cout << "Total " << a.getTotal() << endl;
    return 0;
 }
