3eb6f5ab4a180d5e3f024f89d744a47a4a05172a
杨鹏昊 <13106919+baodaoqianju@user.noreply.gitee.com>
2023-12-13 09:34:04 +0000

    update hb/resolver/build_args_resolver.py.
    Signed-off-by: 杨鹏昊 <13106919+baodaoqianju@user.noreply.gitee.com>

--- hb/resolver/build_args_resolver.py
+++ hb/resolver/build_args_resolver.py
@@ -162,7 +162,7 @@ class BuildArgsResolver(ArgsResolverInterface):
                     for target in target_name.split(','):
                         for item in target_data:
                             if item['parts'] == target[target.index('_') + 1:]:
-                                new_target = config().product + item['build_target']
+                                new_target = item['build_target']
                                 target_list.append(new_target)
                                 break
                         else:
