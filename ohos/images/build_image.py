c6c3b820cfa3f5d502f1444fce27292e0573e670
wenfei6316 <wenfei6316@163.com>
2023-12-19 19:37:05 +0800

    add riscv64 arch
    Signed-off-by: wenfei6316 <wenfei6316@163.com>
    Change-Id: I18b6cb8a0aadd1332b0a98897b0e4c3721e2ff00

--- ohos/images/build_image.py
+++ ohos/images/build_image.py
@@ -46,7 +46,7 @@ def _prepare_root(system_path, target_cpu):
     os.symlink('/system/bin/init', os.path.join(root_dir, 'init'))
     os.symlink('/system/etc', os.path.join(root_dir, 'etc'))
     os.symlink('/vendor', os.path.join(root_dir, 'chipset'))
-    if target_cpu == 'arm64':
+    if target_cpu == 'arm64' or target_cpu == 'riscv64':
         os.symlink('/system/lib64', os.path.join(root_dir, 'lib64'))
     os.symlink('/system/lib', os.path.join(root_dir, 'lib'))
 
