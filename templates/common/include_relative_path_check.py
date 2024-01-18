0ed0d941ddae613be03842d02c992e444d83049e
lichao <lichao74@huawei.com>
2024-01-18 15:44:13 +0800

    去掉引入第三方依赖的pandas,用自带函数替换
    Signed-off-by: lichao <lichao74@huawei.com>

--- templates/common/include_relative_path_check.py
+++ templates/common/include_relative_path_check.py
@@ -19,10 +19,6 @@ import os
 import re
 import glob
 import os.path
-import csv
-
-import pandas as pd
-
 
 
 class Analyzer:
@@ -36,11 +32,15 @@ class Analyzer:
                 path = re.findall('path="(.*?)"', line)[0]
                 component = path.split('/')[-1]
                 path_info[component] = path
+        item_list = list(path_info.keys())
         for component in components:
             if component['component'] in path_info.keys():
                 component['scan_path'] = project + '/' + path_info[component['component']]
+                if (component['component'] in item_list):
+                    item_list.remove(component['component'])
             else:
                 component['scan_path'] = ''
+        print("no scan component :" + " ".join(item_list))
 
     @classmethod
     def __get_components(cls, config: str, project: str):
@@ -120,25 +120,23 @@ class Analyzer:
         print("scan:")
         print([item['scan_path'] for item in components], project_path)
         result = cls.scan_files(components, project_path)
-        df = pd.DataFrame(result)
-        new_order = ['subsystem', 'component', 'file_path', 'line_num', 'include_cmd']
-        df = df[new_order]
-        df.to_csv(output_path, index=False, sep=' ', header=False, quoting=csv.QUOTE_NONE, escapechar="|")
-
+        with open(output_path, "w", encoding="utf-8") as f:
+            for ele in result:
+                items = ele['subsystem'], ele['component'], ele['file_path'], str(ele['line_num']), ele['include_cmd']
+                f.write(" ".join(items) + "\n")
 
 def get_args():
     parser = argparse.ArgumentParser(
         description=f"common_template.\n")
-    parser.add_argument("-c", "--config_path", required=True, type=str,
-                        help="path of config_file", default="")
-    parser.add_argument("-p", "--project_path", type=str, required=True,
-                        help="root path of openharmony. eg: -p ~/openharmony", default="")
-    parser.add_argument("-x", "--xml_path", type=str, required=True,
-                        help="path of ohos.xml", default="")
+    parser.add_argument("-c", "--config_path", required=False, type=str,
+                        help="path of config_file", default="/data01/source/master/vendor/hihope/rk3568/config.json")
+    parser.add_argument("-p", "--project_path", type=str, required=False,
+                        help="root path of openharmony. eg: -p ~/openharmony", default="/data01/source/master")
+    parser.add_argument("-x", "--xml_path", type=str, required=False,
+                        help="path of ohos.xml", default="/data01/source/master/.repo/manifests/ohos/ohos.xml")
     parser.add_argument("-o", "--output_path", required=False, type=str,
         default="include_relative_path.list", help="name of output_json")
     return parser.parse_args()
-
 
 if __name__ == '__main__':
     args = get_args()
