219e8c8b53a6776ebf7ded0c894f9fd739de33fd
h00520221 <huhao51@huawei.com>
2024-02-04 11:05:48 +0000

    Description: build.py 新增参数支持指定使用的python路径 IssueNo:https://gitee.com/openharmony/build/issues/I90RNS?from=project-issue Feature or Bugfix: Feature Binary Source: No Signed-off-by: D0ub1eH<huhao51@huawei.com>

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -39,14 +39,18 @@ def find_top() -> str:
         cur_dir = os.path.dirname(cur_dir)
 
 
-def get_python() -> str:
+def get_python(python_relative_dir: str) -> str:
     topdir = find_top()
-    python_base_dir = os.path.join(topdir, 'prebuilts/python')
+    if python_relative_dir is None:
+        python_relative_dir = 'prebuilts/python'
+    python_base_dir = os.path.join(topdir, python_relative_dir)
+
     if os.path.exists(python_base_dir):
         python_dir = search(python_base_dir, 'python3')
         return os.path.join(python_dir, 'python3')
     else:
-        print("please execute build/prebuilts_download.sh")
+        print("please execute build/prebuilts_download.sh.",
+            "if you used '--python-dir', check whether the input path is valid.")
         sys.exit()
 
 
@@ -67,7 +71,16 @@ def check_output(cmd: str, **kwargs) -> str:
 
 
 def build(path: str, args_list: list) -> str:
-    python_executable = get_python()
+    python_dir = None
+    if "--python-dir" in args_list:
+        index = args_list.index("--python-dir")
+        if index < len(args_list) - 1:
+            python_dir = args_list[index + 1]
+            del args_list[index: index + 2]
+        else:
+            print("-python-dir parmeter missing value.")
+            sys.exit()
+    python_executable = get_python(python_dir)
     cmd = [python_executable, 'build/hb/main.py', 'build'] + args_list
     return check_output(cmd, cwd=path)
 
