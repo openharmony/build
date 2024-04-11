a11fc2d4a151d37995d711ff2e98043f543721a0
xuezhou_yan <yangang20@huawei.com>
2024-04-11 16:44:13 +0800

    issue:#I9FV4W ohos可执行文件和动态库模版新增编译参数kernel_permission_path
    Signed-off-by: xuezhou_yan <yangang20@huawei.com>

---
+++ hb/services/kernel_permission.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#
+# Copyright (c) 2022 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+import sys
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__))))
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__)))))
+
+from exceptions.ohos_exception import OHOSException
+from util.system_util import SystemUtil
+from util.io_util import IoUtil
+from util.log_util import LogUtil
+from scripts.kernel_permission_handle import check_json_file
+
+
+class KernelPermission():
+
+
+    @staticmethod
+    def run(out_path, root_path):
+        log_path = os.path.join(out_path, 'build.log')
+        KernelPermission._execute_kernel_permission_cmd(log_path, out_path, root_path)
+
+
+    @staticmethod
+    def scan_file(out_path):
+        """scan path uild_configs/kernel_permission/
+        return file_list include kernel_permission.json
+        """
+        file_list = []
+        file_path = file_path = os.path.join(out_path, "build_configs/kernel_permission/")
+        for root, subdirs, files in os.walk(file_path):
+            for _filename in files:
+                content = IoUtil.read_json_file(os.path.join(root, _filename))
+                file_list.append(content[0])
+        return file_list
+
+
+    @staticmethod
+    def _execute_kernel_permission_cmd(log_path, out_path, root_path):
+        """execute cmd
+        llvm-object --add-section .kernelpermission=json_file xx/xx.so
+        """
+        exec = KernelPermission._regist_llvm_objcopy_path(root_path)
+        LogUtil.write_log(
+            log_path,
+            "run kernel permission cmd",
+            'info')
+        file_list = KernelPermission.scan_file(out_path)
+        cmds = KernelPermission.gen_cmds(file_list, out_path, exec)
+        for cmd in cmds:
+            LogUtil.write_log(
+                log_path,
+                cmd,
+                'info')
+            SystemUtil.exec_command(
+                cmd,
+                log_path
+                )
+
+
+    @staticmethod
+    def _regist_llvm_objcopy_path(root_path):
+        """find llvm_objcopy_path executable
+        :raise OHOSException: when can't find the llvm_objcopy_path excutable
+        """
+        llvm_objcopy_path = os.path.join(root_path, "prebuilts/clang/ohos/linux-x86_64/llvm/bin/llvm-objcopy")
+        if os.path.exists(llvm_objcopy_path):
+            exec = llvm_objcopy_path
+            return exec
+        else:
+            raise OHOSException(
+                'There is no gn executable file at {}'.format(llvm_objcopy_path), '0001')
+
+
+    @staticmethod
+    def gen_cmds(file_list, out_path, exec):
+        """generate cmd
+        llvm-object --add-section .kernelpermission=json_file xx/xx.so
+        """
+        cmds = []
+        cmd = []
+        for info in file_list: 
+            kernel_permission_file = os.path.join(out_path, info.get("kernel_permission_path"))
+            if not check_json_file(kernel_permission_file):
+                raise OHOSException(
+                    'kernel_permission json file {} error!'.format(kernel_permission_file), '0001')
+            target_name = info.get("target_name")
+            module_info_file = os.path.join(out_path, info.get("module_info_file"))
+            target_info = IoUtil.read_json_file(module_info_file)
+            label_name = target_info.get("label_name")
+            target_source = os.path.join(out_path, target_info.get("source"))
+            if target_name == label_name:
+                cmd = [exec, 
+                        "--add-section", 
+                        ".kernelpermission=" + kernel_permission_file,
+                        target_source
+                        ]
+            cmds.append(cmd)
+        return cmds
+
+
+if __name__ == "__main__":
+    pass
