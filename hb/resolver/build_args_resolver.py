62179c143cbc2a2eb16fbceb834f0e8752004f5a
林昱晔 <linyuye@huawei.com>
2024-06-13 07:27:42 +0000

    tdd精准编译适配
    Signed-off-by: 林昱晔 <linyuye@huawei.com>

--- hb/resolver/build_args_resolver.py
+++ hb/resolver/build_args_resolver.py
@@ -76,6 +76,7 @@ class BuildArgsResolver(ArgsResolverInterface):
             'device_config_path', config.device_config_path)
 
         target_generator.regist_arg('target_cpu', config.target_cpu)
+        target_generator.regist_arg('tdd_branch', config.tdd_branch)
         target_generator.regist_arg(
             'is_{}_system'.format(config.os_level), True)
 
@@ -144,15 +145,29 @@ class BuildArgsResolver(ArgsResolverInterface):
             config.target_os = target_arg.arg_value
 
     @staticmethod
+    def resolve_tdd_branch(target_arg: Arg, build_module: BuildModuleInterface):
+        """resolve '--tdd-branch' arg.
+        :param target_arg: arg object which is used to get arg value.
+        :param build_module [maybe unused]: build module object which is used to get other services.
+        :phase: prebuild.
+        """
+        config = Config()
+        default_build_args = IoUtil.read_json_file(DEFAULT_BUILD_ARGS)
+        if config.tdd_branch == "":
+            config.tdd_branch = target_arg.arg_value
+        elif target_arg.arg_value != default_build_args.get("tdd_branch").get("argDefault"):
+            config.tdd_branch = target_arg.arg_value
+
+    @staticmethod
     def get_tdd_repository(input_file):
         if not os.path.isfile(input_file):
             raise OHOSException(f'{input_file} not found')
-        
+        config = Config()
         target_set = set()
         with open(input_file, 'r') as input_f:
             data = csv.DictReader(input_f)
             for csv_row in data:
-                if csv_row['dayu200_tdd'] == 'Y':
+                if csv_row[config.tdd_branch] == 'Y':
                     target_set.add(csv_row['repoistory'])
         return target_set
 
