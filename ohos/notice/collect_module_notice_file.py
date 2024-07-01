af8be47893a5fb3824880f81e9767e4fa63a2c21
liangxinyan123 <liangxinyan2@huawei.com>
2024-07-01 09:01:43 +0000

    update ohos/notice/collect_module_notice_file.py.
    Signed-off-by: liangxinyan123 <liangxinyan2@huawei.com>

--- ohos/notice/collect_module_notice_file.py
+++ ohos/notice/collect_module_notice_file.py
@@ -103,14 +103,14 @@ def do_collect_notice_files(options, depfiles: str):
             depfiles.append(readme_path)
             notice_file_info = get_license_from_readme(readme_path)
             notice_file = notice_file_info[0]
-            module_notice_info['Software'] = "{} {}".format(notice_file_info[1])
+            module_notice_info['Software'] = "{}".format(notice_file_info[1])
             module_notice_info['Version'] = "{}".format(notice_file_info[2])
 
     if notice_file is None:
         opensource_file = find_opensource_recursively(os.path.abspath(options.module_source_dir))
         if opensource_file is not None and os.path.exists(opensource_file):
             notice_file_info = get_license_from_readme(opensource_file)
-            module_notice_info['Software'] = "{} {}".format(notice_file_info[1])
+            module_notice_info['Software'] = "{}".format(notice_file_info[1])
             module_notice_info['Version'] = "{}".format(notice_file_info[2])
         else:
             module_notice_info['Software'] = ""
