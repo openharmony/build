9d06f391475ea8921593903cff93b2c5cafa1a5f
杨鹏昊 <yangpenghao1@h-partners.com>
2024-07-05 11:26:27 +0000

    fix: Modify format
    Signed-off-by: 杨鹏昊 <yangpenghao1@h-partners.com>

--- config/mac/sdk_info.py
+++ config/mac/sdk_info.py
@@ -16,8 +16,8 @@ import sys
 # "iphoneos" or "macosx" generally).
 
 
-def SplitVersion(version: str or bytes):
-  """Splits the Xcode version to 3 values.
+def split_version(version: str or bytes):
+    """Splits the Xcode version to 3 values.
 
   >>> list(SplitVersion('8.2.1.1'))
   ['8', '2', '1']
@@ -25,15 +25,15 @@ def SplitVersion(version: str or bytes):
   ['9', '3', '0']
   >>> list(SplitVersion('10.0'))
   ['10', '0', '0']
-  """
-  if isinstance(version, bytes):
-    version = version.decode()
-  version = version.split('.')
-  return itertools.islice(itertools.chain(version, itertools.repeat('0')), 0, 3)
+    """
+    if isinstance(version, bytes):
+      version = version.decode()
+    version = version.split('.')
+    return itertools.islice(itertools.chain(version, itertools.repeat('0')), 0, 3)
 
 
-def FormatVersion(version: str or bytes):
-  """Converts Xcode version to a format required for DTXcode in Info.plist
+def format_version(version: str or bytes):
+    """Converts Xcode version to a format required for DTXcode in Info.plist
 
   >>> FormatVersion('8.2.1')
   '0821'
@@ -41,66 +41,77 @@ def FormatVersion(version: str or bytes):
   '0930'
   >>> FormatVersion('10.0')
   '1000'
-  """
-  major, minor, patch = SplitVersion(version)
-  return ('%2s%s%s' % (major, minor, patch)).replace(' ', '0')
+    """
+    major, minor, patch = SplitVersion(version)
+    return ('%2s%s%s' % (major, minor, patch)).replace(' ', '0')
 
 
-def FillXcodeVersion(settings: dict):
-  """Fills the Xcode version and build number into |settings|."""
-  lines = subprocess.check_output(['xcodebuild', '-version']).splitlines()
-  settings['xcode_version'] = FormatVersion(lines[0].split()[-1])
-  settings['xcode_version_int'] = int(settings['xcode_version'], 10)
-  settings['xcode_build'] = lines[-1].split()[-1]
+def fill_Xcode_version(settings: dict) -> dict:
+    """Fills the Xcode version and build number into |settings|."""
+
+    try:
+        lines = subprocess.check_output(['xcodebuild', '-version']).splitlines()
+        settings['xcode_version'] = format_version(lines[0].split()[-1])
+        settings['xcode_version_int'] = int(settings['xcode_version'], 10)
+        settings['xcode_build'] = lines[-1].split()[-1]
+    except subprocess.CalledProcessError as e:  
+          print(f"Failed to run xcodebuild -version: {e}")
+
+    return settings
 
 
-def FillMachineOSBuild(settings: dict):
-  """Fills OS build number into |settings|."""
-  settings['machine_os_build'] = subprocess.check_output(
-      ['sw_vers', '-buildVersion']).strip()
+def fill_machine_OS_build(settings: dict):
+    """Fills OS build number into |settings|."""
+    settings['machine_os_build'] = subprocess.check_output(
+        ['sw_vers', '-buildVersion']).strip()
+    return settings
 
 
-def FillSDKPathAndVersion(settings: dict, platform: str, xcode_version: str or bytes):
-  """Fills the SDK path and version for |platform| into |settings|."""
-  settings['sdk_path'] = subprocess.check_output([
-      'xcrun', '-sdk', platform, '--show-sdk-path']).strip()
-  settings['sdk_version'] = subprocess.check_output([
-      'xcrun', '-sdk', platform, '--show-sdk-version']).strip()
-  settings['sdk_platform_path'] = subprocess.check_output([
-      'xcrun', '-sdk', platform, '--show-sdk-platform-path']).strip()
-  if xcode_version >= '0720':
-    settings['sdk_build'] = subprocess.check_output([
-        'xcrun', '-sdk', platform, '--show-sdk-build-version']).strip()
-  else:
-    settings['sdk_build'] = settings['sdk_version']
+def fill_SDK_path_and_version(settings: dict, platform: str, xcode_version: str or bytes):
+    """Fills the SDK path and version for |platform| into |settings|."""
+    settings['sdk_path'] = subprocess.check_output([
+        'xcrun', '-sdk', platform, '--show-sdk-path']).strip()
+    settings['sdk_version'] = subprocess.check_output([
+        'xcrun', '-sdk', platform, '--show-sdk-version']).strip()
+    settings['sdk_platform_path'] = subprocess.check_output([
+        'xcrun', '-sdk', platform, '--show-sdk-platform-path']).strip()
+    if xcode_version >= '0720':
+      settings['sdk_build'] = subprocess.check_output([
+          'xcrun', '-sdk', platform, '--show-sdk-build-version']).strip()
+    else:
+      settings['sdk_build'] = settings['sdk_version']
+    return settings
 
 
 if __name__ == '__main__':
-  doctest.testmod()
+    doctest.testmod()
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--developer_dir", required=False)
-  args, unknownargs = parser.parse_known_args()
-  if args.developer_dir:
-    os.environ['DEVELOPER_DIR'] = args.developer_dir
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--developer_dir", required=False)
+    args, unknownargs = parser.parse_known_args()
+    if args.developer_dir:
+      os.environ['DEVELOPER_DIR'] = args.developer_dir
 
-  if len(unknownargs) != 1:
-    sys.stderr.write(
-        'usage: %s [iphoneos|iphonesimulator|macosx]\n' %
-        os.path.basename(sys.argv[0]))
-    sys.exit(1)
+    if len(unknownargs) != 1:
+      sys.stderr.write(
+          'usage: %s [iphoneos|iphonesimulator|macosx]\n' %
+          os.path.basename(sys.argv[0]))
+      sys.exit(1)
 
-  settings = {}
-  FillMachineOSBuild(settings)
-  FillXcodeVersion(settings)
-  FillSDKPathAndVersion(settings, unknownargs[0], settings['xcode_version'])
+    settings = {}
+    fill_machine_OS_build(settings)
+    fill_Xcode_version(settings)
+    try:  
+        fill_SDK_path_and_version(settings, 'iphoneos', settings['xcode_version'])  
+    except ValueError as e:  
+        print(f"Error: {e}")
 
-  for key in sorted(settings):
-    value = settings[key]
-    if isinstance(value, bytes):
-      value = value.decode()
-    if key != 'xcode_version_int':
-        value = '"%s"' % value
-        print('%s=%s' % (key, value))
-    else:
-        print('%s=%d' % (key, value))
+    for key in sorted(settings):
+      value = settings[key]
+      if isinstance(value, bytes):
+        value = value.decode()
+      if key != 'xcode_version_int':
+          value = '"%s"' % value
+          print('%s=%s' % (key, value))
+      else:
+          print('%s=%d' % (key, value))
