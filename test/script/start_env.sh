b2ace384b5c5f8d12ec23f6747d482e5e39323b6
luming <luming29@huawei.com>
2024-07-24 11:28:21 +0800

    [Bug] Cleancode warning fix IssueNo: https://gitee.com/openharmony/build/issues/IAF6CZ Signed-off-by: luming <luming29@huawei.com>

--- test/script/start_env.sh
+++ test/script/start_env.sh
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 function install_pytest() {
     if ! command -v pytest &> /dev/null; then
         echo "Installing pytest..."
