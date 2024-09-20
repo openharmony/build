145259663e9e887ef99b9153cac2d50bae7081ff
flying <pengmengjie1@huawei.com>
2024-09-20 09:31:43 +0000

    UP
    Signed-off-by: flying <pengmengjie1@huawei.com>

--- hb/containers/status.py
+++ hb/containers/status.py
@@ -61,11 +61,18 @@ def throw_exception(func):
                 _type = 'UNKNOWN ERROR TYPE'
                 _desc = 'NO DESCRIPTION'
                 _solution = 'NO SOLUTION'
-            if not (sys.argv[1] == 'build' and ("--indep-build" in sys.argv[2:] or "-i" in sys.argv[2:] 
-                                                or (len(sys.argv) == 4 and sys.argv[-1] == "-t"))):
+            if not judge_indep():
                 _print_formatted_tracebak(_code, str(exception), _type, _desc, _solution)
             exit(-1)
     return wrapper
+
+
+def judge_indep():
+    """Description: judge whether it is related to independent build
+    """
+    return sys.argv[1] == 'build' and (
+            "--indep-build" in sys.argv[2:] or "-i" in sys.argv[2:] or sys.argv[-1] == "-t" or (
+            "-t" in sys.argv and sys.argv[sys.argv.index("-t") + 1][0] == '-'))
 
 
 def _print_formatted_tracebak(_code, _exception, _type, _desc, _solution):
