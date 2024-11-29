f19c8d2691374afce8e2983e6aa88b5e87d6fc97
xukun_cai <401117262@qq.com>
2024-11-29 11:30:33 +0800

    utf-8
    Signed-off-by: xukun_cai <401117262@qq.com>

--- scripts/is_substring.py
+++ scripts/is_substring.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # Copyright (c) 2021 Huawei Device Co., Ltd.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
