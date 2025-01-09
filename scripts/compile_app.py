f3b9ff6db02fb17410173ad3034ebb6e2cafecf9
liangxinyan123 <liangxinyan2@huawei.com>
2025-01-09 02:16:39 +0000

    update scripts/compile_app.py.
    Signed-off-by: liangxinyan123 <liangxinyan2@huawei.com>

--- scripts/compile_app.py
+++ scripts/compile_app.py
@@ -112,8 +112,8 @@ def gen_unsigned_hap_path_json(build_profile: str, cwd: str, options):
         for module in modules_list:
             src_path = module.get('srcPath')
             project_name = options.build_profile.replace("/build-profile.json5", "").split("/")[-1]
-            src_path = os.path.join(options.target_out_dir, options.target_app_dir, project_name, src_path)
             if options.test_hap:
+                src_path = os.path.join(options.target_out_dir, options.target_app_dir, project_name, src_path)
                 if options.target_app_dir:
                     unsigned_hap_path = os.path.join(
                         src_path, 'build/default/outputs/ohosTest')
@@ -122,6 +122,7 @@ def gen_unsigned_hap_path_json(build_profile: str, cwd: str, options):
                         cwd, src_path, 'build/default/outputs/ohosTest')
             else:
                 if options.target_app_dir:
+                    src_path = os.path.join(options.target_out_dir, options.target_app_dir, project_name, src_path)
                     unsigned_hap_path = os.path.join(
                         src_path, 'build/default/outputs/default')
                 else:
