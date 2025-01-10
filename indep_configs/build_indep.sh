13b9cf71fbc52854cf40965ce835cd9c4c7a56f5
xietingwei <xietingwei@h-partners.com>
2025-01-10 08:31:16 +0000

    idl_tool加到默认下载
    Signed-off-by: xietingwei <xietingwei@h-partners.com>

--- indep_configs/build_indep.sh
+++ indep_configs/build_indep.sh
@@ -56,6 +56,7 @@ if [ -d "binarys/third_party/rust" ];then
     echo "rust directory exists"
     if [ -d "third_party/rust" ]; then
         echo "third_party/rust exists"
+        rm -rf third_party/rust/crates
         cp -r binarys/third_party/rust/crates third_party/rust
     else
         mkdir -p "third_party/rust"
