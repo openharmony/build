64fc735996ebd20b7309146c138e9242de0a5661
cuican <cuican23@huawei.com>
2025-02-22 17:36:45 +0800

    new: 独立编译 gn和ninja参数, 编译目标参数 传递适配
    Signed-off-by: cuican <cuican23@huawei.com>

--- indep_configs/scripts/utils.py
+++ indep_configs/scripts/utils.py
@@ -28,3 +28,63 @@ def get_json(file_path):
     except Exception as e:
         print(f"{file_path}: \n {e}")
     return data
+
+
+def get_indep_args():
+    """
+    读取独立构建参数的JSON文件，并返回解析后的数据。
+    :return: 包含独立构建参数的字典（成功时）或 None（失败时）
+    """
+    src_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+    file_path = os.path.join(src_root, "out/hb_args/indepbuildargs.json")
+    try:
+        # 以只读模式打开文件，并使用json模块加载内容
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        # 如果文件不存在
+        print(f"file not found：{file_path}")
+        return None
+    except Exception as e:
+        print(f"something wrong with file：{e}")
+        return None
+
+
+def get_ninja_args():
+    """
+    从独立构建参数中获取Ninja构建系统的参数。
+    :return: Ninja参数列表
+    """
+    input_ninja_args = []
+    data = get_indep_args()
+    # 如果配置中设置了保持Ninja运行，则添加相应的参数
+    if data["keep_ninja_going"]["argDefault"]:
+        input_ninja_args.append("-k10000")
+    return input_ninja_args
+
+
+def get_gn_args():
+    """
+    从独立构建参数中获取GN构建系统的参数。
+    :return: GN参数列表
+    """
+    input_gn_args = []
+    data = get_indep_args()
+    # 如果配置中设置了GN参数，则扩展到参数列表中
+    if data["gn_args"]["argDefault"]:
+        input_gn_args.extend(data["gn_args"]["argDefault"])
+
+    return input_gn_args
+
+
+def get_build_target():
+    """
+    从独立构建参数中获取编译目标。
+    :return: 编译目标
+    """
+    input_build_target = []
+    data = get_indep_args()
+    if data["build_target"]["argDefault"]:
+        input_build_target.extend(data["build_target"]["argDefault"])
+    return input_build_target
