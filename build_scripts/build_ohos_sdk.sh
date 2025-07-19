0c60d4616e703b40a8e9c2245f04e651a3207c75
Luming <luming29@huawei.com>
2025-07-19 15:48:19 +0800

    SDK目录清空失败不再退出
    Signed-off-by: Luming <luming29@huawei.com>

--- build_scripts/build_ohos_sdk.sh
+++ build_scripts/build_ohos_sdk.sh
@@ -47,7 +47,7 @@ function prebuilt_sdk() {
             --generate-ninja-trace=false \
             --gn-args "skip_generate_module_list_file=true sdk_platform=${current_platform} ndk_platform=${current_platform} use_cfi=false use_thin_lto=false enable_lto_O0=true sdk_check_flag=false enable_ndk_doxygen=false archive_ndk=false sdk_for_hap_build=true enable_archive_sdk=false enable_notice_collection=false enable_process_notice=false ${_sdk_gn_args[*]}"
         if [[ "$?" -ne 0 ]]; then
-            echo -e "\033[31m[OHOS ERROR] ohos-sdk build failed! \033[0m"
+            echo -e "\033[31m[OHOS ERROR] ohos-sdk build failed! Try using '--no-prebuilts-sdk' to skip.\033[0m"
             exit 1
         fi
         [[ -d "${ROOT_PATH}/prebuilts/ohos-sdk/linux" ]] && rm -rf "${ROOT_PATH}/prebuilts/ohos-sdk/linux"
@@ -56,16 +56,13 @@ function prebuilt_sdk() {
         mkdir -p "${SDK_PREBUILTS_PATH}/linux/native"
         mv "${ROOT_PATH}/out/sdk/sdk-native/os-irrelevant/"* "${SDK_PREBUILTS_PATH}/linux/native/"
         mv "${ROOT_PATH}/out/sdk/sdk-native/os-specific/linux/"* "${SDK_PREBUILTS_PATH}/linux/native/"
-        pushd "${SDK_PREBUILTS_PATH}/linux" > /dev/null || {
-            echo -e "\033[31m[OHOS ERROR] Failed to enter directory: ${SDK_PREBUILTS_PATH}/linux\033[0m"
-            exit 1
-        }
+        pushd "${SDK_PREBUILTS_PATH}/linux" > /dev/null
             mkdir -p "${full_api_version}"
             for dir in */; do
                 [[ -d "${dir}" && "${dir}" != "${full_api_version}/" ]] && mv "${dir}" "${full_api_version}/"
             done
-        popd > /dev/null || exit 1
-    popd > /dev/null || exit 1
+        popd > /dev/null
+    popd > /dev/null
     local target_dir="${source_root_dir}/prebuilts/ohos-sdk/linux/${api_version}/previewer"
     [[ ! -d "${target_dir}" ]] && mkdir -p "${target_dir}"
     cp -r "${source_root_dir}/prebuilts/ohos-sdk/linux/${api_version}/native/oh-uni-package.json" "${target_dir}/" 2>/dev/null
