521a10bfcfd8412dda4b2f4afd31b06bff687963
zhaohang <zhaohang44@huawei.com>
2025-08-14 14:03:33 +0800

    调整下载超时时间
    Signed-off-by: zhaohang <zhaohang44@huawei.com>

--- hb/util/system_util.py
+++ hb/util/system_util.py
@@ -126,8 +126,9 @@ class SystemUtil(metaclass=NoInstance):
 
         ret_code = process.returncode
         if ret_code != 0:
+            cmd_str = " ".join(cmd)
+            LogUtil.hb_error(f"command failed: \"{cmd_str}\" , ret_code: {ret_code}")
             LogUtil.get_failed_log(log_path)
-        return ret_code
 
     @staticmethod
     def get_current_time(time_type: str = 'default'):
