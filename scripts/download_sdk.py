e0fff845e5c8fa52096ec9d63531974402bfe7e8
li-ke1067 <like115@huawei.com>
2025-12-23 11:36:19 +0800

    update
    Signed-off-by: li-ke1067 <like115@huawei.com>

--- scripts/download_sdk.py
+++ scripts/download_sdk.py
@@ -137,7 +137,7 @@ def main():
         x = requests.post(url, json=myobj)
         data = json.loads(x.text)
     except BaseException:
-        Exception("Unable to establish connection with dcp.openharmony.cn")
+        Exception("Unable to establish connection with ci.openharmony.cn")
 
     products_list = data['data']['dailyBuildVos']
     for product in products_list:
@@ -149,7 +149,7 @@ def main():
                 break
 
             if product['obsPath'] and os.path.exists(default_save_path):
-                download_url = 'http://download.dcp.openharmony.cn/{}'.format(product['obsPath'])
+                download_url = 'http://download.ci.openharmony.cn/{}'.format(product['obsPath'])
                 save_path2 = default_save_path
 
             try:
