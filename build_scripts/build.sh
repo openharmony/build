02c46d67340f8a6ec36ac3b411259a6f9c9e4cbf
leokii <liangxinyan2@huawei.com>
2026-08-10 15:26:01 +0800

    IssueNo: https://gitcode.com/openharmony/build/issues/4660 Signed-off-by: leokii <liangxinyan2@huawei.com>

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -132,7 +132,7 @@ npm config set strict-ssl false
 npm config set lockfile false
 cat $HOME/.npmrc | grep 'lockfile=false' > /dev/null || echo 'lockfile=false' >> $HOME/.npmrc > /dev/null
 
-cp -rf ninja ${SOURCE_ROOT_DIR}/prebuilts/build-tools/linux-x86/bin/
+cp -rf ${SOURCE_ROOT_DIR}/ninja ${SOURCE_ROOT_DIR}/prebuilts/build-tools/linux-x86/bin/
 
 function init_ohpm() {
   TOOLS_INSTALL_DIR="${SOURCE_ROOT_DIR}/prebuilts/build-tools/common"
