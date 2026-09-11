9308b8d6a40dcc63ad99dbb9895662a337f6b3b2
leokii <liangxinyan2@huawei.com>
2026-09-11 09:23:34 +0800

    update: 更新文件 rust_linker_wrapper.py
    Signed-off-by: leokii <liangxinyan2@huawei.com>

--- scripts/rust_linker_wrapper.py
+++ scripts/rust_linker_wrapper.py
@@ -29,7 +29,7 @@ def main():
 
     new_args = []
     for arg in args:
-        if arg.startswith("-Wl,--version-script")g:
+        if arg.startswith("-Wl,--version-script"):
             script_path = arg[len("-Wl,--version-script"): ]
             if version_script and os.path.abspath(script_path) == os.path.abspath(version_script):
                 new_args.append(arg)
